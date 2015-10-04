c39115b7e8957a67a6d51df2f9e295dd1e5435f6
Penpo Li <lixinliming@gmail.com>
2015-10-05 02:49:00 +0800

    change mongojs to mongoose

--- public/controller/controller.js
+++ public/controller/controller.js
@@ -1,29 +1,24 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-	console.log('Hello world from controller');
 	// 数据模板
 	var refresh = function(){
 		$http.get('/contactlist').success(function(response){
-			console.log('I get the data I requsted!');
 			$scope.contactlist = response;
+			document.forms["contactform"].reset();
 		});
 	}
 	refresh();
 	$scope.addContact = function(){
-		console.log($scope.contact);
 		$http.post('/contactlist',$scope.contact).success(function(response){
-			console.log(response);
 			refresh();
 		});
 	};
 	$scope.remove = function(id){
-		console.log(id);
 		$http.delete('/contactlist/' + id).success(function(response){
 			refresh();
 		})
 	};
 	$scope.edit = function(id){
-		console.log(id);
 		$http.get('/contactlist/' + id).success(function(response){
 			$scope.contact = response;
 		})
--- server.js
+++ server.js
@@ -2,62 +2,91 @@ var express = require('express');
 var bodyparser = require('body-parser');
 var app = express();
 var mongojs = require('mongojs');
-var db = mongojs('contactlist',['contactlist']);
-
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/contactlist');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+	console.log("Database is connected!");
+});
+var contactSchema = new mongoose.Schema({
+    name: {type: String},
+    email: String,
+    number: String
+});
+var Contact = mongoose.model('Contact', contactSchema);
 app.use(express.static(__dirname + '/public'));
 app.use(bodyparser.json());
 
 app.get('/contactlist', function(req, res){
-	console.log('I received a GET request!');
-	db.contactlist.find(function(err, docs){
-		console.log(docs);
-		res.json(docs);
+	Contact.find(function(err, contacts) {
+		if (err) {
+			return console.err(err);
+		};
+		res.json(contacts);
 	});
 })
 // 接收POST请求
 app.post('/contactlist', function(req, res){
 	console.log(req.body);
-	db.contactlist.insert(req.body, function(err, docs){
-		res.json(docs);
+	// db.contactlist.insert(req.body, function(err, docs){
+	// 	res.json(docs);
+	// });
+	var newcontact = new Contact(req.body);
+	newcontact.save(function(err, contact) {
+		if (err) {
+			return console.log(err)
+		};
+		console.log(contact);
+	});
+	Contact.find(function(err, contacts) {
+		if (err) {
+			return console.err(err);
+		};
+		res.json(contacts);
 	});
 });
 
 // 删除数据
 app.delete('/contactlist/:id', function(req, res){
 	var id = req.params.id;
-	db.contactlist.remove({_id: mongojs.ObjectId(id)}, function(err, docs){
-		res.send(docs);
+	Contact.findByIdAndRemove(id,function(err, contact){
+		if (err) {
+			return console.log(err);
+		};
+		console.log(contact);
+		res.json(contact);
 	})
 })
 
 // 获取单个数据
 app.get('/contactlist/:id', function(req, res){
 	var id = req.params.id;
-	db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function(err, docs){
-		res.send(docs);
+	Contact.findById(id,function(err, contact){
+		if (err) {
+			return console.log(err);
+		};
+		console.log(contact);
+		res.json(contact);
 	})
 })
 // 更改数据
 app.put('/contactlist/:id', function(req, res) {
 	var id = req.params.id;
-	console.log(req.body.name);
-	db.contactlist.findAndModify({
-			query: {
-				_id: mongojs.ObjectId(id)
-			},
-			update: {
-				$set: {
-					name: req.body.name,
-					email: req.body.email,
-					number: req.body.number
-				}
-			},
-			new: true
-		},
-		function(err, docs) {
-			res.json(docs);
-		})
+	Contact.findByIdAndUpdate(id, {
+		$set: {
+			name: req.body.name,
+			email: req.body.email,
+			number: req.body.number
+		}
+		}, function(err, contact){
+			if (err) {
+				return console.log(err);
+			};
+			console.log(contact);
+			res.json(contact);
+	})
 })
 
-app.listen(3000);
+app.listen(2333);
 console.log('Server on port 3000');
