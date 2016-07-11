7296c04a338ded29193128028ce8dd34e36967ce
Penpo Li <lixinliming@gmail.com>
2016-07-11 23:57:39 +0800

    manually delete contact info to database

--- manipulateDB/delete.js
+++ manipulateDB/delete.js
@@ -19,27 +19,21 @@ var contactSchema = new mongoose.Schema({
 });
 
 var Contact = mongoose.model('Contact', contactSchema);
-console.time('add contacts');
-function addContacts(num){
+console.time('delete contacts');
+Contact.find(function(err, contacts) {
+	if (err) {
+		return console.error(err);
+	};
+	removeContacts(contacts);
+});
+function deleteContacts(contacts){
 	if(num <= 0){
-		console.log('manipulate finished');
 		process.exit();
 	}
-	let firstName = (Math.random() * 1e16).toString(36).replace(/\d/g, '');
-	let lastName = (Math.random() * 1e16).toString(36).replace(/\d/g, '');
-
-	let newcontact = new Contact({
-		name: `${firstName[0].toUpperCase() + firstName.substr(1)} ${lastName[0].toUpperCase() + lastName.substr(1)}`,
-		email: `${firstName}.${lastName}@gmail.com`,
-		number: Math.random() * 1e16
-	});
-	newcontact.save(function(err, contact) {
+	Contact.findByIdAndRemove(contacts.id, function(err, contact){
 		if (err) {
-			return console.log(err)
+			return console.log(err);
 		};
-		// console.log(contact);
-		addContacts(num - 1);
 	});
 }
-addContacts(500);
-console.timeEnd('add contacts');
+console.timeEnd('delete contacts');
